7f51c45c16e3b831efabea4674964cb07cfe4e5e
Dennis PC <dennis.strik@student.hu.nl>
2017-01-05 17:43:06 +0100

    constructors attribute rule

--- src/RuleType/AttributeRangeRule.java
+++ src/RuleType/AttributeRangeRule.java
@@ -3,9 +3,13 @@ package RuleType;
 public class AttributeRangeRule implements RuleTypeStrategy {
     private String lhs, rhs, constraint, operator;
     public AttributeRangeRule(String lhs, String rhs, String operator) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+        this.operator = operator;
     }
 
     public AttributeRangeRule(String constraint) {
+        this.constraint = constraint;
     }
 }
 
