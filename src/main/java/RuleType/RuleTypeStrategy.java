bc0f12a106c996c6664495378f3ee753b32ea99f
arthur1472 <admin@ownprojects.info>
2017-01-13 13:33:35 +0100

    .

---
+++ src/main/java/RuleType/RuleTypeStrategy.java
@@ -0,0 +1,10 @@
+package RuleType;
+
+public interface RuleTypeStrategy {
+
+    String leftHandSide = null;
+    String rightHandSide = null;
+    String column = null;
+    String operator = null;
+}
+
