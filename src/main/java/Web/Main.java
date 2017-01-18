f1626386abc4ffc4f47f64badad966ef57229665
arthur1472 <admin@ownprojects.info>
2017-01-18 14:33:02 +0100

    Web side to handle the requests + added json support in java

--- src/main/java/Web/Main.java
+++ src/main/java/Web/Main.java
@@ -1,6 +1,9 @@
 package Web;
-import java.util.Map;
-import java.util.Set;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 
 import static spark.Spark.*;
 /**
@@ -16,6 +19,20 @@ public class Main {
 
         post("/", (request, response) -> {
             String jsonObject = request.body();
+
+            JSONParser parser = new JSONParser();
+            try {
+                Object obj = parser.parse(jsonObject);
+                JSONObject object = (JSONObject) obj;
+                JSONArray ruleparts = (JSONArray) object.get("ruleparts");
+                for (Object rulepart: ruleparts) {
+                    System.out.println(rulepart.toString());
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            //System.out.println(array.get(1));
 
             return jsonObject;
         });
