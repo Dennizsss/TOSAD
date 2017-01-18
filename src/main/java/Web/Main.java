c81fae665252b3381e198eaa9b2ffaaf35697314
arthur1472 <admin@ownprojects.info>
2017-01-18 13:46:58 +0100

    Web side to handle the requests

--- src/main/java/Web/Main.java
+++ src/main/java/Web/Main.java
@@ -1,10 +1,23 @@
 package Web;
+import java.util.Map;
+import java.util.Set;
+
 import static spark.Spark.*;
 /**
  * Created by Arthur on 13-1-2017.
  */
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (request, response) -> "Hello World!");
+        port(8080);
+
+        get("/", (request, response) -> {
+            return "This is not how it works.";
+        });
+
+        post("/", (request, response) -> {
+            String jsonObject = request.body();
+
+            return jsonObject;
+        });
     }
 }
