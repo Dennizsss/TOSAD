d5a8f57043e4fe6302edd33ee93b5280a83e10f5
arthur1472 <admin@ownprojects.info>
2017-01-30 14:04:11 +0100

    Hotfix for patch.

--- RUN BEFORE OPENING PROJECT.bat
+++ RUN BEFORE OPENING PROJECT.bat
@@ -1,10 +1,9 @@
 @echo off
-copy /-y ojdbc7-12.1.0.1.jar %userprofile%\.m2\repository\jdbc-12.1.0.1.jar
-cd %userprofile%\.m2\repository\
-mkdir org\oracle
-mkdir org\oracle\jdbc
-mkdir org\oracle\jdbc\12.1.0.1
 
-move /-y jdbc-12.1.0.1.jar org\oracle\jdbc\12.1.0.1\jdbc-12.1.0.1.jar
+mkdir %userprofile%\.m2\repository\org\oracle
+mkdir %userprofile%\.m2\repository\org\oracle\jdbc
+mkdir %userprofile%\.m2\repository\org\oracle\jdbc\12.1.0.1
+
+copy /-y ojdbc7-12.1.0.1.jar %userprofile%\.m2\repository\org\oracle\jdbc\12.1.0.1
 
 pause
